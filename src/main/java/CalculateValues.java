da467975f463ceb60b063dfc58151c9489a3bd61
harsharnw <harsharnsoffice2@gmail.com>
2021-01-14 18:34:10 +0530

    First commit

---
+++ src/main/java/CalculateValues.java
@@ -0,0 +1,22 @@
+public class CalculateValues {
+
+    public int addValues(int a, int b)
+    {
+        return a+b;
+    }
+
+    public int subtractValues(int a, int b)
+    {
+        return a-b;
+    }
+
+    public int multiplyValues(int a, int b)
+    {
+        return a*b;
+    }
+
+    public int divideValues(int a, int b)
+    {
+        return a/b;
+    }
+}
