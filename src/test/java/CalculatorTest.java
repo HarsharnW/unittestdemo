47002c303e955f9a6b43cc642c0ac7b739a7bdf0
harsharnw <harsharnsoffice2@gmail.com>
2021-01-14 11:19:39 +0530

    commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd()
+    {
+        Assert.assertEquals(70, calc.add(30, 40));
+    }
+
+    @Test
+    public void testSubtract()
+    {
+        Assert.assertEquals(60, calc.subtraction(100, 40));
+    }
+
+    @Test
+    public void testMultiply()
+    {
+        Assert.assertEquals(200, calc.multiply(2, 100));
+    }
+
+    /*
+    @Test
+    public void testDivide()
+    {
+        Assert.assertEquals(50, calc.divide(100, 2));
+    }*/
+}
